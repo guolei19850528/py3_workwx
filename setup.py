dc659210f396e77825d37c45e78cc1b934c7b3d1
guolei <174000902@qq.com>
2024-12-07 17:40:46 +0800

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="py3-workwx",
-    version="1.1.4",
+    version="1.1.6",
     description="The Python3 Work Weixin Library Developed By Guolei",
     long_description=long_description,
     long_description_content_type="text/markdown",
