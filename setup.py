e10ae28fe6c488ac1cb20e7d3ddf8aedf0fea097
guolei <174000902@qq.com>
2024-12-07 17:41:35 +0800

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="py3-workwx",
-    version="1.1.6",
+    version="1.1.7",
     description="The Python3 Work Weixin Library Developed By Guolei",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,7 +25,7 @@ setup(
     author="guolei",
     author_email="174000902@qq.com",
     license="MIT",
-    keywors=["workwx", "work weixin", "企业微信", "群机器人", "服务端API", "webhook"],
+    keywors=["workwx", "work weixin", "企业微信", "群机器人", "服务端API", "webhook", "guolei", "郭磊"],
     packages=setuptools.find_packages('./'),
     install_requires=[
         "py3-requests",
